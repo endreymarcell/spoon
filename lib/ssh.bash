8d7b2a711625a3c132532fcb5c4c3f05de9858de
Marcell Endrey <endrey.marcell@gmail.com>
2019-01-23 23:14:05 +0100

    extend lib/ssh to prepare for VPC

--- lib/ssh.bash
+++ lib/ssh.bash
@@ -1,42 +1,64 @@
 spoon_ssh() {
     node_count=$(echo "${nodes}" | jq '. | length')
     verbose_log "[spoon] number of instances: ${node_count}"
+    if ! is_vpc=$(check_vpc); then
+        echo Cannot mix VPC and non-VPC nodes.
+        exit 1
+    fi
     if [[ "${node_count}" -gt 1 ]]; then
-        ips=$(echo "${nodes}" | jq '.[].publicIp' | tr -d '"' | xargs)
-        verbose_log "[spoon] IP addresses:"
-        [[ "${arg_verbose}" = 1 ]] && for ip in ${ips}; do echo "${ip}"; done
-        ssh_multiple "${ips}"
+        if [[ $is_vpc = 1 ]]; then
+            ssh_multiple_vpc
+        else
+            ssh_multiple_non_vpc
+        fi
     elif [[ "${node_count}" -eq 1 ]]; then
-        ip=$(echo "${nodes}" | jq '.[0].publicIp' | tr -d '"')
-        verbose_log "[spoon] IP address: ${ip}"
-        ssh_single -o StrictHostKeyChecking=no -l root  "${ip}"
+        if [[ $is_vpc = 1 ]]; then
+            ssh_single_vpc
+        else
+            ssh_single_non_vpc
+        fi
     fi
 }
 
-ssh_single() {
-    [[ "${arg_dry_run}" = 1 ]] && return
-    verbose_log "[spoon] calling ssh"
-    if [[ "${arg_docker}" = 1 ]]; then
-        ssh "${@}" -t 'HN=`hostname | cut -f 2 --delimiter=-`; INST_ID=`docker ps | grep $HN-app | cut -f 1 -d " "`; docker exec -ti $INST_ID bash -c '"'"'bash --init-file <(echo ". ../virtualenv/bin/activate")'"'"
+check_vpc() {
+    vpc_node_count=$(echo "${nodes}" | jq 'map(select(.vpc)) | length')
+    non_vpc_node_count=$(echo "${nodes}" | jq 'map(select(.vpc == null)) | length')
+    if [[ $node_count = $vpc_node_count ]]; then
+        echo 1
+    elif [[ $node_count = $non_vpc_node_count ]]; then
+        echo 0
     else
-        ssh "${@}"
+        return 1
     fi
 }
 
-ssh_multiple() {
+ssh_multiple_vpc() {
+    verbose_log All nodes are in VPC.
+    ips=$(echo "${nodes}" | jq '.[].privateIp' | tr -d '"' | xargs)
+    verbose_log "[spoon] IP addresses:"
+    [[ "${arg_verbose}" = 1 ]] && for ip in ${ips}; do echo "${ip}"; done
+    echo to be implemented
+    exit 1
+}
+
+ssh_multiple_non_vpc() {
+    verbose_log None of the nodes are in VPC.
+    ips=$(echo "${nodes}" | jq '.[].publicIp' | tr -d '"' | xargs)
+    verbose_log "[spoon] IP addresses:"
+    [[ "${arg_verbose}" = 1 ]] && for ip in ${ips}; do echo "${ip}"; done
     [[ "${arg_dry_run}" = 1 ]] && return
     check_cssh_availability
     if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
         verbose_log "[spoon] calling i2cssh"
         # I actually need the word splitting here, hence the lack of quotes
         # shellcheck disable=SC2086
-        i2cssh --login root $1
+        i2cssh --login root $ips
         echo hint: press Cmd+Shift+I to send your keyboard input to all the instances
     else
         verbose_log "[spoon] calling csshx"
         # I actually need the word splitting here, hence the lack of quotes
         # shellcheck disable=SC2086
-        csshx --login root $1
+        csshx --login root $ips
     fi
 }
 
@@ -52,5 +74,26 @@ check_cssh_availability() {
             echo Please install csshX to SSH to multiple instances.
             exit 1
         fi
+    fi
+}
+
+ssh_single_vpc() {
+    verbose_log The selected node is in VPC.
+    ip=$(echo "${nodes}" | jq '.[0].privateIp' | tr -d '"')
+    verbose_log "[spoon] IP address: ${ip}"
+    echo to be implemented
+    exit 1
+}
+
+ssh_single_non_vpc() {
+    verbose_log The selected node is not in VPC.
+    ip=$(echo "${nodes}" | jq '.[0].publicIp' | tr -d '"')
+    verbose_log "[spoon] IP address: ${ip}"
+    [[ "${arg_dry_run}" = 1 ]] && return
+    verbose_log "[spoon] calling ssh"
+    if [[ "${arg_docker}" = 1 ]]; then
+        ssh -o StrictHostKeyChecking=no -l root "${ip}" -t 'HN=`hostname | cut -f 2 --delimiter=-`; INST_ID=`docker ps | grep $HN-app | cut -f 1 -d " "`; docker exec -ti $INST_ID bash -c '"'"'bash --init-file <(echo ". ../virtualenv/bin/activate")'"'"
+    else
+        ssh -o StrictHostKeyChecking=no -l root "${ip}"
     fi
 }
